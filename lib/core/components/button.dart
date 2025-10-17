3daa3b9bfeb95ff455594203aea97af7b00713d5
alviennn <alvien.rnp@gmail.com>
2025-10-17 22:00:17 +0700

    membuat button

---
+++ lib/core/components/button.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class MenuButton extends StatefulWidget {
+  final String title;
+  final String imagePath;
+  final VoidCallback onTap;
+
+  const MenuButton({
+    Key? key,
+    required this.title,
+    required this.imagePath,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  State<MenuButton> createState() => _MenuButtonState();
+}
+
+class _MenuButtonState extends State<MenuButton> {
+  bool _isHovered = false; // Status hover
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          width: 140,
+          height: 120,
+          decoration: BoxDecoration(
+            color: _isHovered
+                ? const Color(0xFF007ACC)
+                : const Color(0xFF0096FF),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: [
+              BoxShadow(
+                color: _isHovered ? Colors.black38 : Colors.black26,
+                blurRadius: _isHovered ? 8 : 4,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                widget.imagePath,
+                width: 45,
+                height: 45,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 8),
+              Text(
+                widget.title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 13,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
